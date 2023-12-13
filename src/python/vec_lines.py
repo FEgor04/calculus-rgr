c315519fbf6715e033f20e8090289a0f3ea75d60
Egor Fedorov <fegor2004@gmail.com>
2023-12-13 09:51:55 +0300

    fix(task1.2): исправлена ошибка при нахождении уравнения век. линий

--- src/python/vec_lines.py
+++ src/python/vec_lines.py
@@ -5,7 +5,7 @@ import plot_utils as pu
 
 
 def main(args):
-    # pu.figure_setup()
+    pu.figure_setup()
 
     fig_size = pu.get_fig_size(10, 5)
     fig = plt.figure()
@@ -16,14 +16,19 @@ def main(args):
     total_x = []
     total_y = []
 
-    for C in [0.1, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 48.0, 64.0, 128]:
+    def compute_y(x, C):
+        return - np.log(C - np.exp(-x))
+
+
+    c_range = [np.exp(i) for i in range(1, 10)]
+    for C in c_range:
         x = np.linspace(- np.log(C) + eps, max_x, 1000)
-        y = np.log(C - np.exp(- x))
+        y = compute_y(x, C)
         ax.plot(x, y, c='b', lw=pu.plot_lw())
 
-        X = np.linspace(- np.log(C) + eps, max_x, 10)
+        X = np.linspace(- np.log(C) + eps, max_x, 20)
         total_x += list(X)
-        total_y += list(np.log(C - np.exp(-X)))
+        total_y += list(compute_y(X, C))
         
 
     total_x = np.array(total_x)
@@ -31,15 +36,16 @@ def main(args):
 
     U = np.exp(total_x)
     V = - np.exp(total_y)
+    # N = np.sqrt(np.square(U) + np.square(V))
 
-    q = ax.quiver(total_x, total_y, U, V, angles='xy', scale_units='xy')
-    ax.quiverkey(q, X=0.3, Y=1.1, U=10, label=r"$\vec H$", labelpos='E')
+    q = ax.quiver(total_x, total_y, U/N, V/N, angles='xy', scale_units='xy')
+    # ax.quiverkey(q, X=0.3, Y=1.1, U=10, label=r"$\vec H$", labelpos='E')
 
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
 
-    plt.ylim([-15, 15])
-    plt.xlim([-6, 6])
+    # plt.ylim([-15, 15])
+    # plt.xlim([-6, 6])
 
     ax.set_axisbelow(True)
 
