a3fbdb0bee9710be06b480c00240f07de0c0ece5
Egor Fedorov <fegor2004@gmail.com>
2023-12-27 13:32:08 +0300

    fix(task3): исправлен пример с окружностью

--- src/python/conformal_circle.py
+++ src/python/conformal_circle.py
@@ -10,24 +10,24 @@ from conformal_utils import w
 
 def main(args):
     fig, (ax_virtual, ax_physical) = cu.setup()
-    r = np.sqrt(2) * 2
+    r = np.sqrt(2)
     sampling_rate = 100_000
     u_virtual_1 = np.linspace(-r, r, sampling_rate)
-    v_virtual_1 = np.sqrt(r**2 - u_virtual_1 ** 2)
+    v_virtual_1 = np.sqrt(r**2 - u_virtual_1 ** 2) + 1
     u_virtual_2 = np.array(u_virtual_1[::-1])
-    v_virtual_2 = -np.sqrt(r**2 - u_virtual_2 ** 2)
+    v_virtual_2 = -np.sqrt(r**2 - u_virtual_2 ** 2) + 1
     u_virtual = np.concatenate([u_virtual_1, u_virtual_2])
     v_virtual = np.concatenate([v_virtual_1, v_virtual_2])
     virtual_numbers = np.array([complex(u_virtual[i], v_virtual[i]) for i in range(len(u_virtual))])
 
-    def plot_complex(ax, numbers, color):
+    def plot_complex(ax, numbers):
         x = [z.real for z in numbers]
         y = [z.imag for z in numbers]
 
-        ax.plot(x, y, color=color)
+        ax.plot(x, y)
 
-    plot_complex(ax_virtual, virtual_numbers, 'orange')
-    plot_complex(ax_physical, w(virtual_numbers), 'orange')
+    plot_complex(ax_virtual, virtual_numbers)
+    plot_complex(ax_physical, w(virtual_numbers))
     
     if args.save:
         pu.save_fig(fig, args.save)
